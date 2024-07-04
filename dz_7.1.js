afcd5479cb9027f31757099bd4385f6c5cca8db1
Anna23041984 <savchenkoanna2304@gmail.com>
2024-07-04 14:26:29 +0200

    dz_7.1

--- dz_7.1.js
+++ dz_7.1.js
@@ -2,9 +2,9 @@
 
 
 const getAdd = function () { let res = 0; return function(x) { res += x; return res; } };
+
 let sum = getAdd();
 
-console.log(sum(1)); // 1
-console.log(sum(3)); // 4
+console.log(sum(1)); 
+console.log(sum(3));
   
-
